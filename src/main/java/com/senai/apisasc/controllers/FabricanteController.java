22dec181a3a6c93d55881f91e4a262d2a941e019
Gabriel Trindade <tr.ndade3@gmail.com>
2023-11-14 23:38:43 -0300

    fabri

--- src/main/java/com/senai/apisasc/controllers/FabricanteController.java
+++ src/main/java/com/senai/apisasc/controllers/FabricanteController.java
@@ -1,16 +1,16 @@
 package com.senai.apisasc.controllers;
 
 
+import com.senai.apisasc.dtos.FabricanteDto;
 import com.senai.apisasc.models.EnderecoModel;
 import com.senai.apisasc.models.FabricanteModel;
 import com.senai.apisasc.repositories.FabricanteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,5 +36,17 @@ public class FabricanteController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(fabricanteBuscado.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> cadastrarFabricante(@RequestBody @Valid FabricanteDto fabricanteDto) {
+        if (fabricanteRepository.findByCnpj(fabricanteDto.cnpj()) != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Esse CNPJ ja esta cadastrado!");
+        }
+
+        FabricanteModel fabricante = new FabricanteModel();
+        BeanUtils.copyProperties(fabricanteDto, fabricante);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(fabricanteRepository.save(fabricante));
     }
 }
--- src/main/java/com/senai/apisasc/repositories/FabricanteRepository.java
+++ src/main/java/com/senai/apisasc/repositories/FabricanteRepository.java
@@ -1,9 +1,11 @@
 package com.senai.apisasc.repositories;
 
+import com.senai.apisasc.models.EnderecoModel;
 import com.senai.apisasc.models.FabricanteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface FabricanteRepository extends JpaRepository<FabricanteModel, UUID> {
+    FabricanteModel findByCnpj(String cnpj);
 }
